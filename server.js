13c3de512c8741f2a481bb80842c27fc8b85c417
Aaron Dweck <aarondweck24@gmail.com>
2025-02-09 14:36:54 +0000

    added a logger middleware to server

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import mongoSanitize from 'express-mongo-sanitize'
+import 'dotenv/config'
+import logger from './middleware/logger.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(mongoSanitize())
+app.use(logger)
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
+
+mongoose.connect(process.env.MONGODB_URI)
