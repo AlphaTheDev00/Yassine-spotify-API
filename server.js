011620cc2c7795047e0716e36ca70a40f7a9bc04
Aaron Dweck <aarondweck24@gmail.com>
2025-02-09 15:06:07 +0000

    add errorHandler file to server

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ import mongoose from 'mongoose'
 import mongoSanitize from 'express-mongo-sanitize'
 import 'dotenv/config'
 import logger from './middleware/logger.js'
+import errorHandler from './middleware/errorHandler.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(mongoSanitize())
 app.use(logger)
+
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
