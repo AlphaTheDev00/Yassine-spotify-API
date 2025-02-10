54cfb7c047b6b7eeee452e2f59aaad31c00b8ac5
Aaron Dweck <aarondweck24@gmail.com>
2025-02-10 11:45:39 +0000

    added basic song creation

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,17 @@
 import express from 'express'
 import Song from '../Models/Song.js'
+import validateToken from '../middleware/validateToken.js'
 
 const router = express.Router()
+
+router.post('/api/songs', validateToken ,async (req, res, next) => {
+    try {
+        req.body.user_id = req.user._id
+        const song = await Song.create(req.body)
+        res.json({message: 'created song', song: song})
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default router
